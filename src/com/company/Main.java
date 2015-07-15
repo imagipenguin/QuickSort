5b7ab247840d9f59399540d85c7efb1dd4b9eb8a
imagipenguin <kmjones2@gmail.com>
2015-07-14 22:34:20 -0700

    Just the basics

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,66 +7,14 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static long countinv = 0;
-
     public static void log(Object args) {
         System.out.println(args.toString());
     }
 
     public static void test_mergesort(int[] args){
-        Main.countinv=0;
-        int[] i = mergesort(args);
-        log("The number of inversions is "+countinv);
+
     }
 
-
-    public static int[] merge(int[] a, int[] b) {
-        //log("Begin Merge");
-        int n = a.length + b.length;
-        //log("The length of the array is " + n);
-        int[] merge = new int[n];
-        int i = 0;
-        int j = 0;
-        for (int k = 0; k < n; k++) {
-            if (i < a.length && j < b.length) {
-                if (a[i] <= b[j]) {
-                    merge[k] = a[i];
-                    i++;
-                } else {
-                    merge[k] = b[j];
-                    j++;
-                    Main.countinv += a.length - i;
-                }
-            } else {
-                if (j < b.length) {
-                    merge[k] = b[j];
-                    j++;
-                } else {
-                    merge[k] = a[i];
-                    i++;
-                }
-            }
-            //log("The value of i is " + i + " and the value of j is " + j);
-            //log(merge[k]);
-        }
-        return merge;
-    }
-
-    public static int[] mergesort(int[] args) {
-        int l = args.length;
-        //log("the length is "+l);
-        //log("the halfway is "+(l-1)/2);
-        if (l > 1) {
-            int[] a = Arrays.copyOfRange(args, 0, (l - 1) / 2 + 1);
-            int[] b = Arrays.copyOfRange(args, (l - 1) / 2 + 1, l);
-            args = merge(mergesort(a), mergesort(b));
-        }
-
-
-        //System.out.println(Arrays.toString(a));
-        //System.out.println(Arrays.toString(b));
-        return args;
-    }
 
     public static void main(String[] args) {
         // write your code here
@@ -80,47 +28,9 @@ public class Main {
         int[] bb = new int[]{2};
         int[] aaa = new int[]{7, 3, 1, 5};
         int[] testcase = new int[]{1, 3, 5, 2, 4, 6};
-        //int[] test = merge(bb, aa);
-        /*
-        int[] testcaseforum = new int[] { 9, 12, 3, 1, 6, 8, 2, 5, 14, 13, 11, 7, 10, 4, 0 };
-        int[] testms = mergesort(testcaseforum);
-        log(Main.countinv);
-        Main.countinv = 0;
 
-        int[] testms2 = mergesort(b);
-        log(Main.countinv);
-        Main.countinv = 0;
-        int[] testms3 = mergesort(testcase);
-        */
 
-        int[] arr = data();
-        int[] testcaseprogressive1 = Arrays.copyOfRange(arr, 0, 10000);
-        int[] testcaseprogressive2 = Arrays.copyOfRange(arr, 0, 20000);
-        int[] testcaseprogressive3 = Arrays.copyOfRange(arr, 0, 30000);
-        int[] testcaseprogressive4 = Arrays.copyOfRange(arr, 0, 40000);
-        int[] testcaseprogressive5 = Arrays.copyOfRange(arr, 0, 50000);
-        int[] testcaseprogressive6 = Arrays.copyOfRange(arr, 0, 60000);
-        int[] testcaseprogressive7 = Arrays.copyOfRange(arr, 0, 70000);
-        int[] testcaseprogressive8 = Arrays.copyOfRange(arr, 0, 80000);
-        int[] testcaseprogressive9 = Arrays.copyOfRange(arr, 0, 90000);
-        int[] testcaseprogressive10 = Arrays.copyOfRange(arr, 0, 100000);
-        test_mergesort(testcaseprogressive1);
-        test_mergesort(testcaseprogressive2);
-        test_mergesort(testcaseprogressive3);
-        test_mergesort(testcaseprogressive4);
-        test_mergesort(testcaseprogressive5);
-        test_mergesort(testcaseprogressive6);
-        test_mergesort(testcaseprogressive7);
-        test_mergesort(testcaseprogressive8);
-        test_mergesort(testcaseprogressive9);
-        test_mergesort(testcaseprogressive10);
-
-        //\log(Arrays.toString(arr));
-        //log(arr[3]);
-        //log(arr[3]+1);
-        //int[] sortdata = mergesort(testcaseprogressive);
-        //System.out.println(Arrays.toString(sortdata));
-        //log("The number of inversions is " + Main.countinv + ".");
+        // int[] arr = data();
     }
 
     public static int[] data() {
